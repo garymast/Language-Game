b831ee18e8ce8094d0866ac42a16c29fb36e0ac1
garymast <>
2023-05-26 16:32:44 +0000

    Add random square function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,18 @@ let squares = document.getElementsByClassName("square");
 let mole = document.getElementsByClassName("mole");
 let timeLeft = document.getElementById("time-left");
 let score = document.getElementById("score");
+
+let result = 0;
+
+function randomSquare() {
+    for (let square of squares) {
+        square.classList.remove('mole');
+    }
+
+    let randomSquare = squares[Math.floor(Math.random() * 3)];
+    randomSquare.classList.add('mole');
+
+    // Ensure that the random square isn't the previous mole square
+}
+
+randomSquare();
