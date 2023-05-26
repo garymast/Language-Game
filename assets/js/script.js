0851c19fcdf8979c3124a49d2810314281d88407
garymast <>
2023-05-26 16:13:18 +0000

    Add initial variables for game logic

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+let squares = document.getElementsByClassName("square");
+let mole = document.getElementsByClassName("mole");
+let timeLeft = document.getElementById("time-left");
+let score = document.getElementById("score");
