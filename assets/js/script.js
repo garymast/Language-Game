3754879f0cfae4bf6267f4ee0dc712bd085a2e18
garymast <>
2023-05-26 16:53:04 +0000

    Add the event listener and score counter functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ let timeLeft = document.getElementById("time-left");
 let score = document.getElementById("score");
 
 let result = 0;
+let hitPosition;
 
 function randomSquare() {
     for (let square of squares) {
@@ -13,7 +14,24 @@ function randomSquare() {
     let randomSquare = squares[Math.floor(Math.random() * 3)];
     randomSquare.classList.add('mole');
 
+    hitPosition = randomSquare.id;
     // Ensure that the random square isn't the previous mole square
 }
 
-randomSquare();
+for (let square of squares) {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++;
+            score.textContent = result;
+            hitPosition = null;
+        }
+    })
+}
+
+function moveMole() {
+    let timerId = null;
+    timerId = setInterval(randomSquare, 750)
+}
+
+moveMole();
+// Attach this to a start game button
